9b8a0448d8247886e7321297e2991449d3be2c1d
puc-void <215346186+puc-void@users.noreply.github.com>
2026-06-07 18:29:09 +0600

    file location fixed

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-document.getElementById("login-btn").addEventListener("click",function () {
-            const numberInput= document.getElementById("input-number")
-            const contactNumber= numberInput.value;
-            console.log(contactNumber);
+document.getElementById("login-btn").addEventListener("click", function () {
+    const numberInput = document.getElementById("input-number")
+    const contactNumber = numberInput.value;
+    console.log(contactNumber);
 
 
-            const inputPin= document.getElementById("input-pin")
-            const pinNumber= inputPin.value;
-            console.log(pinNumber)
-            
+    const inputPin = document.getElementById("input-pin")
+    const pinNumber = inputPin.value;
+    console.log(pinNumber)
 
-            if(contactNumber=="01891632464" && pinNumber=="2468"){
-                alert("Login Success!")
-                window.location.assign("/home.html")
-            }
-            else{
-                alert("Login Failed")
-                return;
-            }
+
+    if (contactNumber == "01891632464" && pinNumber == "2468") {
+        alert("Login Success!")
+        window.location.assign("./home.html");
+    }
+    else {
+        alert("Login Failed")
+        return;
+    }
 });
